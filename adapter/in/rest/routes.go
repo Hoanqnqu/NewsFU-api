eafe15f3f6edd2444f1fe29bdf6e6582a8b2658c
Nhat Hoang <nhathoangthuychaub1@gmail.com>
2024-06-18 03:40:03 +0700

    fix

--- adapter/in/rest/routes.go
+++ adapter/in/rest/routes.go
@@ -18,13 +18,13 @@ func AppRouter(
 	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
 	router.Use(cors.Handler(cors.Options{
 		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"https://*", "http://*", "https://newsfu.netlify.app"},
+		AllowedOrigins: []string{"https://*", "http://*"},
 
 		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
+		AllowCredentials: false,
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
 	}))
 
