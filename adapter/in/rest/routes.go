1d21688e039f8c0069fb00d8460706a50eea6236
Nhat Hoang <nhathoangthuychaub1@gmail.com>
2024-06-18 19:07:32 +0700

    fix

--- adapter/in/rest/routes.go
+++ adapter/in/rest/routes.go
@@ -16,18 +16,7 @@ func AppRouter(
 	commentHandler *CommentHandler) *chi.Mux {
 	router := chi.NewRouter()
 	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
-	router.Use(cors.Handler(cors.Options{
-		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"*"},
-
-		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
-	}))
-
+	router.Use(cors.AllowAll().Handler)
 	router.Use(Logger)
 
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
